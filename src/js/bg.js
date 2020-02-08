dc5eb327a816d74d84b275d1524966d0987c837f
papadog218 <32761630+papadog218@users.noreply.github.com>
2020-02-08 19:25:04 +0900

    수정중

--- src/js/bg.js
+++ src/js/bg.js
@@ -3,19 +3,18 @@ const body = document.querySelector("body"),
 
 
 
-function handleImgLoad() {
-    console.log("img is ready");
-} // 원격에서 하는게 아니면 필요없다고 지워버림;;
+// function handleImgLoad() {
+//     console.log("img is ready");
+// } // 원격에서 하는게 아니면 필요없다고 지워버림;;
 
 function paintImg(imgNum) {
     const img = new Image();
     img.src = `src/img/${imgNum}.jpg`;
     img.classList.add("bgImg");
     // appendChild()는 해당 태그안에 제일 밑에 생성된다 (제일 위에는 prepend())
-    // body.prepend(img);
-    body.prepend(img);
+    body.prepend(img); 
     // 원격에서 하는게 아니면 필요없다고 지워버림;;
-    img.addEventListener("loadend", handleImgLoad); 
+    // img.addEventListener("loadend", handleImgLoad); 
 }
 function getRandom() {
     const num = Math.floor(Math.random()*IMG_NUM) + 1;
