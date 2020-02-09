042a1ea683f4747fc664a4503e78777de1ae66ad
papadog218 <32761630+papadog218@users.noreply.github.com>
2020-02-09 18:07:40 +0900

    test...

--- src/js/bg.js
+++ src/js/bg.js
@@ -1,21 +1,16 @@
 const body = document.querySelector("body"),
     IMG_NUM = 5;
 
-
-
-function handleImgLoad(e) {
-    console.log("img is ready");
-    console.log(e);
-}
-
 function paintImg(imgNum) {
     const img = new Image();
     img.src = `src/img/${imgNum}.jpg`;
     img.classList.add("bgImg");
     // appendChild()는 해당 태그안에 제일 밑에 생성된다 (제일 위에는 prepend())
-    // body.prepend(img);
 
-    img.addEventListener("load", handleImgLoad);
+    img.addEventListener("load", function(){
+        console.log("img is ready");
+        body.prepend(img);
+    });
 }
 function getRandom() {
     const num = Math.floor(Math.random()*IMG_NUM) + 1;
