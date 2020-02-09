0c76e7d83be0754cd6c4fdea138644651aeb0d4b
papadog218 <32761630+papadog218@users.noreply.github.com>
2020-02-09 18:24:24 +0900

    디자인 조금 손봄

--- src/js/weather.js
+++ src/js/weather.js
@@ -9,7 +9,7 @@ function getWeather(lat, lon) {
         return res.json();
     }).then(function(json){
         // console.log(json)
-        const temp= json.main.temp;
+        const temp = json.main.temp;
         const place = json.name;
         weather.innerText = `${temp} @ ${place}`;
     })
